5d3b00f08694a28d539be848d8fae062a4c43a81
Midona <midona.rhel@gmail.com>
2025-11-18 18:29:16 +0100

    Fixed reset

--- CustomizePlus/Templates/TemplateEditorManager.cs
+++ CustomizePlus/Templates/TemplateEditorManager.cs
@@ -254,7 +254,9 @@ public class TemplateEditorManager : IDisposable
                 break;
             case BoneAttribute.Scale:
                 if ((resetValue == CurrentlyEditedTemplate!.Bones[boneName].Scaling) &&
-                    (defaultPropagationState == CurrentlyEditedTemplate!.Bones[boneName].PropagateScale))
+                    (defaultPropagationState == CurrentlyEditedTemplate!.Bones[boneName].PropagateScale) &&
+                    (Vector3.One == CurrentlyEditedTemplate!.Bones[boneName].ChildScaling) &&
+                    (true == CurrentlyEditedTemplate!.Bones[boneName].ChildScalingLinked))
                     return false;
                 break;
         }
